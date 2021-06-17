b750bf6d9f91c182b9843a1847e6aa05f84bc437
Zachary Robinson <zachary_robinson@icloud.com>
2021-06-17 13:25:31 -0400

    Formatting with Prettier

--- script.js
+++ script.js
@@ -20,22 +20,22 @@ async function getData() {
 			let element = {
 				question: obj[i].question,
 				answer: obj[i].answer,
-				category: obj[i].categories[0]
+				category: obj[i].categories[0],
 			};
 			output.push(element);
 		}
 		// output
-		console.log("Fetching new questions from randomtriviagenerator.com")
+		console.log("Fetching new questions from randomtriviagenerator.com");
 		return output;
 	} catch {
 		// creates the link
-		let link = "questions/set" + Math.floor(Math.random()*256) + ".txt";
+		let link = "questions/set" + Math.floor(Math.random() * 256) + ".txt";
 		// fetches the data
 		const response = await fetch(link);
 		// converts to json
 		const sets = await response.json();
 		// output
-		console.log("Fetching new questions from predefined folder")
+		console.log("Fetching new questions from predefined folder");
 		return sets;
 	}
 }
@@ -46,20 +46,20 @@ function newQuestion() {
 	if (currentSet.length > 0) {
 		// if there are questions, update screen
 
-		currentIndex = Math.floor(Math.random()*currentSet.length);
+		currentIndex = Math.floor(Math.random() * currentSet.length);
 
-		console.log(`Displaying question ${currentIndex}. There are ${currentSet.length-1} questions left.`)
+		console.log(`Displaying question ${currentIndex}. There are ${currentSet.length - 1} questions left.`);
 
 		document.getElementById("text-question").innerHTML = currentSet[currentIndex]["question"];
 		document.getElementById("text-answer").innerHTML = currentSet[currentIndex]["answer"];
-		document.getElementById("text-category").innerHTML = currentSet[currentIndex]["category"].charAt(0).toUpperCase() + currentSet[currentIndex]["category"].slice(1);
+		document.getElementById("text-category").innerHTML =
+			currentSet[currentIndex]["category"].charAt(0).toUpperCase() + currentSet[currentIndex]["category"].slice(1);
 		document.getElementById("icon-category").src = "assets/categories/" + currentSet[currentIndex]["category"] + ".svg";
 
 		currentSet.splice(currentIndex, 1);
-
 	} else {
 		// otherwise, get a set of questions and try again
-		getData().then(array => {
+		getData().then((array) => {
 			currentSet = array;
 			currentIndex = -1;
 			newQuestion();
@@ -69,34 +69,41 @@ function newQuestion() {
 
 function numToPoints(number) {
 	let suffix;
-	if (number == 1) { suffix = " point"}
-	else { suffix = " points"}
+	if (number == 1) {
+		suffix = " point";
+	} else {
+		suffix = " points";
+	}
 	return number + suffix;
 }
-
-
 
 //CSS Animation functions
 
 function shrink(name) {
-	let target = document.getElementById(name)
-	try { target.classList.remove("box-grow"); }
-	finally { target.classList.add("box-shrink"); }
+	let target = document.getElementById(name);
+	try {
+		target.classList.remove("box-grow");
+	} finally {
+		target.classList.add("box-shrink");
+	}
 }
 
 function grow(name) {
-	let target = document.getElementById(name)
-	try { target.classList.remove("box-shrink"); }
-	finally { target.classList.add("box-grow"); }
+	let target = document.getElementById(name);
+	try {
+		target.classList.remove("box-shrink");
+	} finally {
+		target.classList.add("box-grow");
+	}
 }
 
 function show(name) {
-	let target = document.getElementById(name)
+	let target = document.getElementById(name);
 	target.style.display = "block";
 }
 
 function hide(name) {
-	let target = document.getElementById(name)
+	let target = document.getElementById(name);
 	target.style.display = "none";
 }
 
@@ -112,7 +119,6 @@ function flyOut() {
 	finally { target.classList.add("flyOut"); }
 } */
 
-
 // correct button
 
 document.getElementById("button-large-correct").addEventListener("click", function () {
@@ -127,16 +133,13 @@ document.getElementById("button-large-correct").addEventListener("click", functi
 	document.getElementById("text-points-team-b").innerHTML = numToPoints(teams.teamB.points);
 
 	newQuestion();
-
-})
-
-
+});
 
 // setup
 
 newQuestion();
 
-if (Math.floor(Math.random()*2) == 0) {
+if (Math.floor(Math.random() * 2) == 0) {
 	currentTeam = "teamA";
 	shrink("box-team-b");
 } else {
@@ -144,12 +147,9 @@ if (Math.floor(Math.random()*2) == 0) {
 	shrink("box-team-a");
 }
 
-
-
 // wrong button
 
 document.getElementById("button-large-wrong").addEventListener("click", function () {
-
 	newQuestion();
 
 	if (currentTeam == "teamA") {
@@ -161,10 +161,7 @@ document.getElementById("button-large-wrong").addEventListener("click", function
 		shrink("box-team-b");
 		grow("box-team-a");
 	}
-
 });
-
-
 
 // skip button
 
@@ -172,19 +169,20 @@ document.getElementById("button-large-skip").addEventListener("click", function 
 	newQuestion();
 });
 
-
-
 // start button
 
 document.getElementById("button-setup-start").addEventListener("click", function () {
-
 	// get values from name inputs
 	teams.teamA.name = document.getElementById("teamANameInput").value;
 	teams.teamB.name = document.getElementById("teamBNameInput").value;
 
 	// set values for names if blank
-	if (teams.teamA.name == "") { teams.teamA.name = "Team A"; }
-	if (teams.teamB.name == "") { teams.teamB.name = "Team B"; }
+	if (teams.teamA.name == "") {
+		teams.teamA.name = "Team A";
+	}
+	if (teams.teamB.name == "") {
+		teams.teamB.name = "Team B";
+	}
 
 	// display team points
 	document.getElementById("text-points-team-a").innerHTML = numToPoints(teams.teamA.points);
